8d9f55d3de3b868d035af832f463c11af404de9b
Henzo Sousa <sousahenzo80@gmail.com>
2026-07-29 23:05:37 -0300

    feat: implementa sistema de mensagens de sucesso e erro via sessão PHP

---
+++ Crud_Mundo/includes/mensagens.php
@@ -0,0 +1,12 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if (isset($_SESSION['sucesso'])) {
+    echo '<div class="alert alert-success">' . htmlspecialchars($_SESSION['sucesso']) . '</div>';
+    unset($_SESSION['sucesso']);
+}
+if (isset($_SESSION['erro'])) {
+    echo '<div class="alert alert-danger">' . htmlspecialchars($_SESSION['erro']) . '</div>';
+    unset($_SESSION['erro']);
+}
