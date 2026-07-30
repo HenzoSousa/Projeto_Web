40612d818cfd6dec2a650b3e61da0a5aa5f7a494
Henzo Sousa <sousahenzo80@gmail.com>
2026-07-29 23:05:00 -0300

    feat: cria footer com copyright dinâmico e inclusão dos scripts JavaScript

---
+++ Crud_Mundo/includes/footer.php
@@ -0,0 +1,10 @@
+        </div>
+    </main>
+    <footer class="main-footer">
+        <p>&copy; <?php echo date('Y'); ?> - CRUD Mundo - Desenvolvimento de Sistemas (Etec)</p>
+    </footer>
+    <script src="/CRUD_Mundo/js/script.js"></script>
+    <script src="/CRUD_Mundo/js/validacoes.js"></script>
+    <script src="/CRUD_Mundo/js/confirmacoes.js"></script>
+</body>
+</html>
