e39a9e374381362ce883eaa7d0490fc573ce2d9f
Henzo Sousa <sousahenzo80@gmail.com>
2026-07-29 23:04:40 -0300

    feat: implementa menu de navegação responsivo entre dashboard e módulos

---
+++ Crud_Mundo/includes/menu.php
@@ -0,0 +1,9 @@
+<nav class="main-navigation">
+    <ul>
+        <li><a href="/CRUD_Mundo/paginas/dashboard.php">📊 Dashboard</a></li>
+        <li><a href="/CRUD_Mundo/paginas/continentes/listar.php">🗺️ Continentes</a></li>
+        <li><a href="/CRUD_Mundo/paginas/paises/listar.php">🏳️ Países</a></li>
+        <li><a href="/CRUD_Mundo/paginas/cidades/listar.php">🏙️ Cidades</a></li>
+        <li><a href="/CRUD_Mundo/paginas/governantes/listar.php">👤 Governantes</a></li>
+    </ul>
+</nav>
