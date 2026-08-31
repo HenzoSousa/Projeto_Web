c1d8345614b49e008ec3ac99a5ba8eb4a01dc1a9
Henzo Sousa <sousahenzo80@gmail.com>
2026-08-31 01:02:53 -0300

    fix (menu) logout na tela

--- Crud_Mundo/includes/menu.php
+++ Crud_Mundo/includes/menu.php
@@ -1,3 +1,5 @@
+<?php require_once __DIR__ . '/auth.php'; ?>
+
 <nav class="main-navigation">
     <ul>
         <li><a href="/CRUD_Mundo/paginas/dashboard.php">📊 Dashboard</a></li>
@@ -5,5 +7,9 @@
         <li><a href="/CRUD_Mundo/paginas/paises/listar.php">🏳️ Países</a></li>
         <li><a href="/CRUD_Mundo/paginas/cidades/listar.php">🏙️ Cidades</a></li>
         <li><a href="/CRUD_Mundo/paginas/governantes/listar.php">👤 Governantes</a></li>
+        
+        <?php if (estaLogado()): ?>
+            <li><a href="/CRUD_Mundo/logout.php" style="color:#ff9999;">🚪 Sair (<?= htmlspecialchars($_SESSION['usuario_nome']) ?>)</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
