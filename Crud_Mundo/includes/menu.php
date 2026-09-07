95ad642eb829ab2d0ace6b9510a3702a8dc28927
Henzo Sousa <sousahenzo80@gmail.com>
2026-09-07 16:08:06 -0300

    feat: adiciona link 'Alterar Senha' no menu para usuários logados

--- Crud_Mundo/includes/menu.php
+++ Crud_Mundo/includes/menu.php
@@ -2,14 +2,15 @@
 
 <nav class="main-navigation">
     <ul>
-        <li><a href="/CRUD_Mundo/paginas/dashboard.php">📊 Dashboard</a></li>
-        <li><a href="/CRUD_Mundo/paginas/continentes/listar.php">🗺️ Continentes</a></li>
-        <li><a href="/CRUD_Mundo/paginas/paises/listar.php">🏳️ Países</a></li>
-        <li><a href="/CRUD_Mundo/paginas/cidades/listar.php">🏙️ Cidades</a></li>
-        <li><a href="/CRUD_Mundo/paginas/governantes/listar.php">👤 Governantes</a></li>
+        <li><a href="/CRUD_Mundo/paginas/dashboard.php"> Dashboard</a></li>
+        <li><a href="/CRUD_Mundo/paginas/continentes/listar.php"> Continentes</a></li>
+        <li><a href="/CRUD_Mundo/paginas/paises/listar.php"> Países</a></li>
+        <li><a href="/CRUD_Mundo/paginas/cidades/listar.php"> Cidades</a></li>
+        <li><a href="/CRUD_Mundo/paginas/governantes/listar.php"> Governantes</a></li>
         
         <?php if (estaLogado()): ?>
-            <li><a href="/CRUD_Mundo/logout.php" style="color:#ff9999;">🚪 Sair (<?= htmlspecialchars($_SESSION['usuario_nome']) ?>)</a></li>
+            <li><a href="/CRUD_Mundo/paginas/auth/manutencao_senha.php">Alterar Senha</a></li>
+            <li><a href="/CRUD_Mundo/logout.php" style="color:#ff9999;">Sair (<?= htmlspecialchars($_SESSION['usuario_nome']) ?>)</a></li>
         <?php endif; ?>
     </ul>
 </nav>
